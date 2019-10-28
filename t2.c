5078a26a24cce7c3a68119101af7941187d22b44
BK202 <45834794+xzj478@users.noreply.github.com>
2019-10-28 14:16:07 +0200

    Add files via upload

--- t2.c
+++ t2.c
@@ -4,9 +4,8 @@
 int main(){
 	int n;
 	double ec = 0;
-	double err = exp(1) - ec;
-	for(n = 1; err < 0.001; n++){
-		ec = pow((double)(1 + 1/n),(double)n);}
+	for(n = 1; exp(1) - ec < 0.001; n++){
+		ec = pow((1 +(double)(1/n)),n);}
 	printf("%d\n", n);
 
 	return 0;
