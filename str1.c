53b9f43871d58a6c8df86ad2e7ea4d7ca7ced658
BK202 <45834794+xzj478@users.noreply.github.com>
2019-11-04 14:05:53 +0200

    Add files via upload

---
+++ str1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void hmw(char str[]){
+	int i, j = 0;
+	for(i = 0; str[i] != '\0'; i++){
+		if(str[i] = ' '){
+			j++;}
+	}
+	j++;
+	printf("The sentecnce contain %d words.\n", j);
+}
+
+void lew(char str[]){
+	int i, n=0, m=0;
+	int *l;
+	for(i = 0; str[i] != '\0'; i++){
+		if(str[i] = ' '){
+			*(p+n) = i - m;
+			m = i;
+			n++;}
+	}
+	*(p+n) = i - m;
+	for(j = 0; j < n+1; j++){
+		printf("%d ", *(p+n));}
+	printf("\n");
+}
+
+void nol(char str[]){
+	
+}
