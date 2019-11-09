db1c77d4d017f2e91baf54b865158b78c66f5f91
BK202 <45834794+xzj478@users.noreply.github.com>
2019-11-09 18:37:27 +0200

    Add files via upload

--- t2.c
+++ t2.c
@@ -5,7 +5,7 @@ int main(){
 	int n;
 	double ec=0;
 	for(n=1; fabs(exp(1)-ec) > 0.001; n++){
-        	ec = pow(1+(double)1/n, n);}
+        ec = pow(1+(double)1/n, n);}
 	printf("%d\n", n+1);
 
 	return 0;
